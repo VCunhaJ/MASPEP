b83bbdf25e149f9ed5ee70ff13fc5b7d6529704a
Junior Cunha <vcunha@wpi.edu>
2015-09-06 18:40:38 -0400

    Initial Commit

---
+++ MASPEP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MASPEP
+//
+//  Created by Junior Cunha on 9/6/15.
+//  Copyright (c) 2015 Vanderlei Cunha Jr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
