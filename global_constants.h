0084fa8e79ca4afc4cb83595287276e12fa4e5d3
Junior Cunha <vcunha@wpi.edu>
2015-09-06 18:49:28 -0400

    File that contains Global preprocessors and constants

---
+++ global_constants.h
@@ -0,0 +1,27 @@
+//
+//  global_constants.h
+//  MASPEP
+//
+//  Created by Junior Cunha on 9/6/15.
+//  Copyright (c) 2015 Vanderlei Cunha Jr. All rights reserved.
+//
+
+#ifndef MASPEP_global_constants_h
+#define MASPEP_global_constants_h
+
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <math.h>
+
+
+/*OpenCV Libraries*/
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/videoio/videoio.hpp>
+
+
+
+#endif
